f261d5f969b2880a1b4506dd21ca2818a2cc0708
turgodi <hugo.dalboussiere@gmail.com>
2018-09-24 23:16:04 +0200

    finalize listings CRUD v1

--- lmc_back/src/app.js
+++ lmc_back/src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const serveStatic = require('serve-static');
+const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// parse application/json
+app.use(bodyParser.json());
 
 // serve front-end application
 app.use('/', serveStatic(path.join(__dirname, '../..', 'lmc_front/dist')));
@@ -12,6 +19,8 @@ const router = express.Router();
 /// LISTINGS ROUTES ///
 router.get('/listings', listingsController.index);
 router.get('/listings/:id', listingsController.getListing);
+router.delete('/listings/:id', listingsController.deleteListing);
+router.post('/listings/:id', listingsController.updateListing);
 router.put('/listings', listingsController.createListing);
 
 app.use('/api', router);
--- lmc_back/src/controllers/listings-controller.js
+++ lmc_back/src/controllers/listings-controller.js
@@ -7,7 +7,7 @@ exports.index = function(req, res) {
 	res.send(listings);
 };
 
-// get a single listing
+// get a listing
 exports.getListing = function(req, res) {
 	const id = Number.parseInt(req.params.id);
 	const listings = JSON.parse(fs.readFileSync(LISTING_FILE));
@@ -22,16 +22,75 @@ exports.getListing = function(req, res) {
 	}
 };
 
+// delete a listing
+exports.deleteListing = function(req, res) {
+	const id = Number.parseInt(req.params.id);
+	const listings = JSON.parse(fs.readFileSync(LISTING_FILE));
+
+	let index = -1;
+
+	for (let i = 0; i < listings.length; i++) {
+		const listing = listings[i];
+		if (listing.id === id) {
+			index = i;
+			break;
+		}
+	}
+
+	if (index !== -1) {
+		listings.splice(index, 1);
+		fs.writeFileSync(LISTING_FILE, JSON.stringify(listings));
+		res.send(listings);
+	} else {
+		res.status(404).send('Not found');
+	}
+};
+
+// update a listing
+exports.updateListing = function(req, res) {
+	const id = Number.parseInt(req.params.id);
+	const newListing = req.body || {};
+	const listings = JSON.parse(fs.readFileSync(LISTING_FILE));
+
+	let index = -1;
+
+	for (let i = 0; i < listings.length; i++) {
+		const listing = listings[i];
+		if (listing.id === id) {
+			index = i;
+			break;
+		}
+	}
+
+	if (index !== -1) {
+		listings[index].title = newListing.title || '';
+		listings[index].description = newListing.description || '';
+		listings[index].email = newListing.email || '';
+		listings[index].phoneNumber = newListing.phoneNumber || '';
+		fs.writeFileSync(LISTING_FILE, JSON.stringify(listings));
+		res.send(listings);
+	} else {
+		res.status(404).send('Not found');
+	}
+};
+
 // create a listing
 exports.createListing = function(req, res) {
+	const newListing = req.body || {};
 	const listings = JSON.parse(fs.readFileSync(LISTING_FILE));
+
+	let newId = 0;
+	if (listings.length > 0) {
+		newId = listings[listings.length - 1].id + 1
+	}
+
 	const listing = {
-		id: -1, // todo
+		id: newId,
 		userId: -1, // todo
-		title: "Title",
-		description: "Description",
-		email: "Email",
-		phoneNumber: "Phone number"
+		title: newListing.title || '',
+		description: newListing.description || '',
+		email: newListing.email || '',
+		phoneNumber: newListing.phoneNumber || ''
 	};
 	listings.push(listing);
 	fs.writeFileSync(LISTING_FILE, JSON.stringify(listings));
