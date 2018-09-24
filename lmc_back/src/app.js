0b125b778a4a6c13f6b98f73d44496a08b2f8919
turgodi <hugo.dalboussiere@gmail.com>
2018-09-24 23:24:44 +0200

    clean misc

--- lmc_back/src/app.js
+++ lmc_back/src/app.js
@@ -13,7 +13,10 @@ app.use(bodyParser.json());
 // serve front-end application
 app.use('/', serveStatic(path.join(__dirname, '../..', 'lmc_front/dist')));
 
+// import controllers
 const listingsController = require('./controllers/listings-controller');
+
+// initialize the router
 const router = express.Router();
 
 /// LISTINGS ROUTES ///
@@ -22,6 +25,8 @@ router.get('/listings/:id', listingsController.getListing);
 router.delete('/listings/:id', listingsController.deleteListing);
 router.post('/listings/:id', listingsController.updateListing);
 router.put('/listings', listingsController.createListing);
+
+/// USERS ROUTES /// todo
 
 app.use('/api', router);
 
